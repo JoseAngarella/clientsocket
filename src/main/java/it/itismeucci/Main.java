ea2f5bd82bbf04dfe76745d2b68032b6318dd26d
lai2-24 <lai2-24@itismeucci.com>
2024-10-09 09:51:51 +0200

    funzionante ma con bug

--- src/main/java/it/itismeucci/Main.java
+++ src/main/java/it/itismeucci/Main.java
@@ -6,19 +6,26 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws UnknownHostException, IOException {
         try {
+            String s="";
             Socket mySocket = new Socket("localhost", 5673);
             System.out.println("il client si è collegato");
             BufferedReader in = new BufferedReader(new InputStreamReader(mySocket.getInputStream()));
             DataOutputStream out = new DataOutputStream(mySocket.getOutputStream());
-            out.writeBytes("ciao che bello essere collegati insieme \n");
-            
-            String stringaCambiatadalServer=in.readLine();
-            System.out.println("nuova stringa= "+stringaCambiatadalServer);
+            Scanner inputConsole = new Scanner(System.in);
 
+            do  {
+
+                s = inputConsole.nextLine();
+                out.writeBytes(s+" \n");
+                
+                String stringaCambiatadalServer=in.readLine();
+                System.out.println("nuova stringa= "+stringaCambiatadalServer);
+            }while((s!="0000"));
             mySocket.close();
 
         } catch (Exception e) {
